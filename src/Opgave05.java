1de7864d7786598446dc56d78b1f5b9a4bf8e65d
August Juel Aagaard <augustjaagaard@gmail.com>
2024-09-16 21:11:10 +0200

    O(n) solution for assignment 5

--- src/Opgave05.java
+++ src/Opgave05.java
@@ -4,40 +4,28 @@ import java.util.Comparator;
 public class Opgave05 {
     public static void main(String[] args) {
         char[] letters = {'W', 'R', 'G', 'W', 'W', 'R', 'G', 'R', 'W', 'W', 'R', 'G', 'W'};
+        System.out.println(Arrays.toString(letters));
         order(letters);
         System.out.println(Arrays.toString(letters));
     }
 
     public static void order(char[] letters) {
-        quickSort(letters, 0, letters.length - 1, new CharComparator());
-    }
-
-    public static void quickSort(char[] letters, int low, int high, Comparator comparator) {
-        if (low < high) {
-            int middle = partition(letters, low, high, comparator);
-            quickSort(letters, low, middle - 1, comparator);
-            quickSort(letters, middle + 1, high, comparator);
-        }
-    }
-
-    private static int partition(char[] letters, int low, int high, Comparator comparator) {
-        char e = letters[low];
-        int i = low + 1;
-        int j = high;
-        while (i <= j) {
-            if (comparator.compare(letters[i], e) <= 0) {
-                i++;
-            } else if (comparator.compare(letters[j], e) > 0) {
-                j--;
+        int g = 0;
+        int r = letters.length - 1;
+        int i = 0;
+        while (i < r) {
+            if (letters[i] == 'G') {
+                swap(letters, g, i);
+                g++;
+            } else if (letters[i] == 'R') {
+                swap(letters, r, i);
+                r--;
             } else {
-                swap(letters, i, j);
                 i++;
-                j--;
             }
         }
-        swap(letters, low, j);
-        return j;
     }
+
 
     private static void swap(char[] letters, int k, int l) {
         char e = letters[k];
